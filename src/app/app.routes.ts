4d41c6bbe7f827b529fec0a2246e66f3cbaf1f5e
DelioCoder <davideveriwhere@gmail.com>
2025-05-24 15:51:00 -0500

    Admin & auth routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
+import { isAdminGuard } from '@auth/guards/is-admin.guard';
+import { notAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
     {
+        path: 'admin',
+        loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
+        canMatch: [isAdminGuard]
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes'),
+        // TODO: Guards
+        canMatch: [notAuthenticatedGuard]
+    },
+    {
         path: '',
         loadChildren: () => import('./store-front/store-front.routes')
-    }
+    },
 ];
