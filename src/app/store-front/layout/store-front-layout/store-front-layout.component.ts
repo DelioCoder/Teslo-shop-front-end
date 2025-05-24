6838e32d14174283c9e0d2d5ef603f45d8adf400
DelioCoder <davideveriwhere@gmail.com>
2025-05-24 15:59:28 -0500

    Store-pages added

--- src/app/store-front/layout/store-front-layout/store-front-layout.component.ts
+++ src/app/store-front/layout/store-front-layout/store-front-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FrontNavbarComponent } from "../../components/front-navbar/front-navbar.component";
+import 'animate.css';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FrontNavbarComponent, FooterComponent],
   templateUrl: './store-front-layout.component.html',
   styles: ``
 })
