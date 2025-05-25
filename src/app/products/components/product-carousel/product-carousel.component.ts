ea353fd610e162c72511c94f01aab426e8daf407
DelioCoder <davideveriwhere@gmail.com>
2025-05-24 23:12:22 -0500

    Swiper with images fixed

--- src/app/products/components/product-carousel/product-carousel.component.ts
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, input, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, OnChanges, SimpleChanges, viewChild } from '@angular/core';
 // import Swiper JS
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
@@ -22,17 +22,43 @@ import { ProductImagePipe } from '@products/pipes/product-image.pipe';
     }
   `
 })
-export class ProductCarouselComponent implements AfterViewInit {
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
 
   images = input.required<string[]>();
 
   swiperDiv = viewChild.required<ElementRef>('swiperDiv');
 
+  swiper: Swiper|undefined = undefined;
+
+  // Listen changes to the images input
+  // and reinitialize the swiper
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['images'].firstChange) {
+      return;
+    }
+    if( !this.swiper) return;
+
+    this.swiper.destroy(true, true);
+
+    const paginationEl: HTMLDivElement = this.swiperDiv().nativeElement.querySelector('.swiper-pagination');
+
+    paginationEl.innerHTML = '';
+
+    setTimeout(() => {
+      this.swiperInit();
+    }, 100);
+
+  }
+
   ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+  swiperInit() {
     const element = this.swiperDiv().nativeElement;
     if (!element) return;
 
-    new Swiper(element, {
+    this.swiper = new Swiper(element, {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
